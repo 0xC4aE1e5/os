a41e811daf7eafe1f8de4dbcc93cdbb9f9046612
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-10-19 11:19:42 -0600

    Add files via upload

--- shell/script.js
+++ shell/script.js
@@ -1,17 +1,32 @@
-function viewApps() {
-    if (!document.getElementById("apps").style.display != "none") {
-        document.getElementById("apps").style.display = "block";
-        document.getElementById("wallpaper").style.display = "none";
-    } else {
-        document.getElementById("apps").style.display = "none";
-        document.getElementById("wallpaper").style.display = "block";
-    }
-}
-
 function web() {
-    window.open("https://lite.duckduckgo.com/lite", "", "width=800,height=600")
+    open("https://lite.duckduckgo.com/lite", "", "width=800,height=600")
 }
 
 function dropbox() {
-    window.open("https://dropbox.com", "", "width=800,height=600")
+    open("https://dropbox.com", "", "width=800,height=600")
+}
+
+function build() {
+    document.getElementById("appMUI").style.display = "block";
+}
+
+function generate() {
+    if (localStorage.getItem("apps") == null) {
+        var apps = {};
+        apps[document.getElementById("appName").value] = document.getElementById("appLink").value;
+        localStorage.setItem("apps", JSON.stringify(apps));
+    }
+    else {
+        var apps = JSON.parse(localStorage.getItem("apps"));
+        apps[document.getElementById("appName").value] = document.getElementById("appLink").value;      
+        localStorage.setItem("apps", JSON.stringify(apps));
+    }
+    var apps = JSON.parse(localStorage.getItem("apps"));
+    apps[document.getElementById("appName").value] = document.getElementById("appLink").value;
+    location.reload();
+}
+
+for (const element of Object.keys(JSON.parse(localStorage.getItem("apps")))) {
+    apps = JSON.parse(localStorage.getItem("apps"));
+    document.querySelector("tr").innerHTML += `<td onclick="window.open('${apps[element]}', '', 'width=800,height=600')"><img src="icons/jigsaw.png"><center>${element}</center></td>`
 }
