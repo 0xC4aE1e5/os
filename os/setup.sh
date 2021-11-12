1dc6f7382fc7b236bd92e5d11af35f052421b131
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-11-12 14:32:36 -0700

    Update setup.sh

--- os/setup.sh
+++ os/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Setting up webOS, this may take a while."
+echo "Setting up OS, this may take a while."
 echo "You will most likely see a dialog that mentions display managers. In that dialog, press enter, then select 'sddm'. The rest of the installation is automated. Also, when logging in, make sure the session is Openbox. That will be on the top left."
 sleep 5
 sudo apt install openbox sddm subversion -y
@@ -9,10 +9,10 @@ wget "https://download-chromium.appspot.com/dl/Linux_x64?type=snapshots" -O /tmp
 unzip chromium.zip -d /opt
 popd
 pushd /
-sudo svn checkout https://github.com/ionllc/webOS.git/trunk/shell
-sudo mv shell webOS
+sudo svn checkout https://github.com/ionllc/os.git/trunk/shell
+sudo mv shell os
 popd
 cat > /etc/xdg/openbox/autostart <<EOF
 xsetroot -solid #000000
-/opt/chrome-linux/chrome --kiosk file:///webOS/index.html
+/opt/chrome-linux/chrome --kiosk file:///os/index.html
 EOF
